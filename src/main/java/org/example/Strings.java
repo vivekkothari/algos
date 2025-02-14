819824efffb6317a0b4dd837e5b23d8da246e09f
Vivek Kothari <5404922+vivekkothari@users.noreply.github.com>
2025-02-14 15:40:57 +0530

    more algos

--- src/main/java/org/example/Strings.java
+++ src/main/java/org/example/Strings.java
@@ -229,7 +229,10 @@ class Strings {
     //    System.out.println(numberOfSpecialChars("abBCab"));
     //    System.out.println(numberOfSpecialCharsII("cCceDC"));
     //    System.out.println(lengthOfLastWord("Hello World"));
-    System.out.println(reversePrefix("abcdefd", 'd'));
+    //    System.out.println(reversePrefix("abcdefd", 'd'));
+    //    System.out.println(maxRepeating("aaabaaaabaaabaaaabaaaabaaaabaaaaba", "aaaba"));
+    //    System.out.println(convert("AB", 1));
+    System.out.println(myAtoi("-91283472332"));
   }
 
   // I c e C r e A m
@@ -1026,5 +1029,270 @@ class Strings {
       }
     }
     return res.toArray(String[]::new);
+  }
+
+  /**
+   * https://leetcode.com/problems/maximum-repeating-substring/
+   *
+   * <pre>
+   *   For a string sequence, a string word is k-repeating if word concatenated k
+   *   times is a substring of sequence. The word's maximum k-repeating value is the
+   *   highest value k where word is k-repeating in sequence. If word is not a substring
+   *   of sequence, word's maximum k-repeating value is 0.
+   *
+   * Given strings sequence and word, return the maximum k-repeating value of word in sequence.
+   *
+   * Example 1:
+   *
+   * Input: sequence = "ababc", word = "ab"
+   * Output: 2
+   * Explanation: "abab" is a substring in "ababc".
+   * Example 2:
+   *
+   * Input: sequence = "ababc", word = "ba"
+   * Output: 1
+   * Explanation: "ba" is a substring in "ababc". "baba" is not a substring in "ababc".
+   * Example 3:
+   *
+   * Input: sequence = "ababc", word = "ac"
+   * Output: 0
+   * Explanation: "ac" is not a substring in "ababc".
+   * </pre>
+   */
+  public static int maxRepeating(String sequence, String word) {
+    int k = 0;
+    var curr = new StringBuilder(word);
+    while (sequence.contains(curr)) {
+      curr.append(word);
+      k++;
+    }
+    return k;
+  }
+
+  /**
+   * https://leetcode.com/problems/longest-palindromic-substring/solutions/3598120/longest-palindromic-substring/
+   *
+   * <pre>
+   *   Given a string s, return the longest palindromic substring in s.
+   *
+   * Example 1:
+   *
+   * Input: s = "babad"
+   * Output: "bab"
+   * Explanation: "aba" is also a valid answer.
+   * Example 2:
+   *
+   * Input: s = "cbbd"
+   * Output: "bb"
+   *
+   *
+   * Constraints:
+   *
+   * 1 <= s.length <= 1000
+   * s consist of only digits and English letters.
+   * </pre>
+   */
+  public static String longestPalindrome(String s) {
+    int resLen = 0, resIdx = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+      // odd length
+      int l = i, r = i;
+      while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+        if (r - l + 1 > resLen) {
+          resIdx = l;
+          resLen = r - l + 1;
+        }
+        l--;
+        r++;
+      }
+
+      // even length
+      l = i;
+      r = i + 1;
+      while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+        if (r - l + 1 > resLen) {
+          resIdx = l;
+          resLen = r - l + 1;
+        }
+        l--;
+        r++;
+      }
+    }
+
+    return s.substring(resIdx, resIdx + resLen);
+  }
+
+  /**
+   * https://leetcode.com/problems/zigzag-conversion/
+   *
+   * <pre>
+   *   The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this:
+   *   (you may want to display this pattern in a fixed font for better legibility)
+   *
+   * P   A   H   N
+   * A P L S I I G
+   * Y   I   R
+   * And then read line by line: "PAHNAPLSIIGYIR"
+   *
+   * Write the code that will take a string and make this conversion given a number of rows:
+   *
+   * string convert(string s, int numRows);
+   *
+   *
+   * Example 1:
+   *
+   * Input: s = "PAYPALISHIRING", numRows = 3
+   * Output: "PAHNAPLSIIGYIR"
+   * Example 2:
+   *
+   * Input: s = "PAYPALISHIRING", numRows = 4
+   * Output: "PINALSIGYAHRPI"
+   * Explanation:
+   * P     I    N
+   * A   L S  I G
+   * Y A   H R
+   * P     I
+   * Example 3:
+   *
+   * Input: s = "A", numRows = 1
+   * Output: "A"
+   * </pre>
+   */
+  public static String convert(String s, int numRows) {
+    if (numRows == 1 || numRows >= s.length()) {
+      return s;
+    }
+    List<List<Character>> rows = new ArrayList<>();
+    // init all lists
+    for (int i = 0; i < numRows; i++) {
+      rows.add(new ArrayList<>());
+    }
+    int idx = 0, d = 1;
+    for (char c : s.toCharArray()) {
+      rows.get(idx).add(c);
+      if (idx == 0) {
+        d = 1;
+      } else if (idx == numRows - 1) {
+        d = -1;
+      }
+      idx += d;
+    }
+    StringBuilder result = new StringBuilder();
+    for (List<Character> row : rows) {
+      for (char c : row) {
+        result.append(c);
+      }
+    }
+    return result.toString();
+  }
+
+  /**
+   * https://leetcode.com/problems/string-to-integer-atoi/
+   *
+   * <pre>
+   *   Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer.
+   *
+   * The algorithm for myAtoi(string s) is as follows:
+   *
+   * Whitespace: Ignore any leading whitespace (" ").
+   * Signedness: Determine the sign by checking if the next character is '-' or '+', assuming positivity if neither present.
+   * Conversion: Read the integer by skipping leading zeros until a non-digit character is encountered or the end of the string is reached. If no digits were read, then the result is 0.
+   * Rounding: If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then round the integer to remain in the range. Specifically, integers less than -231 should be rounded to -231, and integers greater than 231 - 1 should be rounded to 231 - 1.
+   * Return the integer as the final result.
+   *
+   *
+   *
+   * Example 1:
+   *
+   * Input: s = "42"
+   *
+   * Output: 42
+   *
+   * Explanation:
+   *
+   * The underlined characters are what is read in and the caret is the current reader position.
+   * Step 1: "42" (no characters read because there is no leading whitespace)
+   *          ^
+   * Step 2: "42" (no characters read because there is neither a '-' nor '+')
+   *          ^
+   * Step 3: "42" ("42" is read in)
+   *            ^
+   * Example 2:
+   *
+   * Input: s = " -042"
+   *
+   * Output: -42
+   *
+   * Explanation:
+   *
+   * Step 1: "   -042" (leading whitespace is read and ignored)
+   *             ^
+   * Step 2: "   -042" ('-' is read, so the result should be negative)
+   *              ^
+   * Step 3: "   -042" ("042" is read in, leading zeros ignored in the result)
+   *                ^
+   * Example 3:
+   *
+   * Input: s = "1337c0d3"
+   *
+   * Output: 1337
+   *
+   * Explanation:
+   *
+   * Step 1: "1337c0d3" (no characters read because there is no leading whitespace)
+   *          ^
+   * Step 2: "1337c0d3" (no characters read because there is neither a '-' nor '+')
+   *          ^
+   * Step 3: "1337c0d3" ("1337" is read in; reading stops because the next character is a non-digit)
+   *              ^
+   * Example 4:
+   *
+   * Input: s = "0-1"
+   *
+   * Output: 0
+   *
+   * Explanation:
+   *
+   * Step 1: "0-1" (no characters read because there is no leading whitespace)
+   *          ^
+   * Step 2: "0-1" (no characters read because there is neither a '-' nor '+')
+   *          ^
+   * Step 3: "0-1" ("0" is read in; reading stops because the next character is a non-digit)
+   *           ^
+   * Example 5:
+   *
+   * Input: s = "words and 987"
+   *
+   * Output: 0
+   *
+   * Explanation:
+   *
+   * Reading stops at the first non-digit character 'w'.
+   * </pre>
+   */
+  public static int myAtoi(String s) {
+    int i = 0, n = s.length(), sign = 1, ans = 0;
+
+    while (i < n && s.charAt(i) == ' ') {
+      i++;
+    }
+
+    if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+      sign = (s.charAt(i) == '-') ? -1 : 1;
+      i++;
+    }
+    while (i < n && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+      int digit = s.charAt(i) - '0';
+      try {
+        var temp = Math.multiplyExact(ans, 10);
+        ans = Math.addExact(temp, digit);
+      } catch (ArithmeticException ignore) {
+        return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+      }
+      i++;
+    }
+
+    return ans * sign;
   }
 }
