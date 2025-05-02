3c3dba334b72326227e694d9c220078de7cdf558
vivekkothari <vivekkothari2304@gmail.com>
2025-05-03 03:40:58 +0530

    more algos

--- src/main/java/org/example/Arrays.java
+++ src/main/java/org/example/Arrays.java
@@ -3169,4 +3169,27 @@ public class Arrays {
     }
     return res;
   }
+
+  /**
+   * https://leetcode.com/problems/valid-parenthesis-string/
+   */
+  public boolean checkValidString(String s) {
+    int min = 0, max = 0;
+    for (var ch : s.toCharArray()) {
+      if (ch == '(') {
+        min++;
+        max++;
+      } else if (ch == ')') {
+        min--;
+        max--;
+      } else {
+        // its a *
+        min--;
+        max++;
+      }
+      if (min < 0) min = 0;
+      if (max < 0) return false;
+    }
+    return min == 0;
+  }
 }
